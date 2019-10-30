337f0593b0d45a5e0b6bcbc5295a43864cc6ed72
Abhi Shah <ashah244@uic.edu>
2019-10-30 07:38:53 -0500

    trying out service worker

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
 importScripts('/IT-202-Project-2/cache-polyfill.js');
-self.addEventListener('install', e => {
+
+
+self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('hi').then(function(cache){
       return cache.addAll([
         '/IT-202-Project-2',
         '/IT-202-Project-2/index.html',
-        '/IT-202-Project-2/page_content/about.html',
+        '/IT-202-Project-2/page_content/_about.html',
         '/IT-202-Project-2/js/list.js',
         '/IT-202-Project-2/js/search.js',
         '/IT-202-Project-2/js/map.js'
